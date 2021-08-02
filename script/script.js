8a81ec8c9343b7adcb1783d32d82958e7492bfbc
jieun1126 <87401167+jieun1126@users.noreply.github.com>
2021-08-03 02:32:52 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.accordion-menu>ul>li').hover(function(){
+        $(this).find(".submenu").stop().slideDown(500);
+    },
+    function(){
+        $(this).find(".submenu").stop().slideUp(500);
+    }
+    );
+});
